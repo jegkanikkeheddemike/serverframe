6c142d74340e0a88cf9ec5638251f0f2e8ad444a
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2023-05-28 21:21:32 +0200

    format

--- backend/src/main/java/dtu/mennesker/ServerFrame/ServerFrame.java
+++ backend/src/main/java/dtu/mennesker/ServerFrame/ServerFrame.java
@@ -5,7 +5,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 import java.util.UUID;
